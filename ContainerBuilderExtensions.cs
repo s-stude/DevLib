9718aa8c751a4e7d31c354dc5a9dbd44ad20ca56
Akim Khalilov <Akim.Khalilov@gmail.com>
2013-03-05 22:54:39 +0200

    Adding IOC Registry assemblies

---
+++ ContainerBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using DevLib.Domain;
+using NHibernate;
+
+namespace DevLib.Infrastructure.NHibernate.AutofacRegistry
+{
+    public static class ContainerBuilderExtensions
+    {
+        public static ContainerBuilder RegisterInfrastructureNHibernateComponents(this ContainerBuilder builder)
+        {
+            builder.RegisterType<PerRequestSessionProvider>().As<ISessionProvider>();
+
+            builder.RegisterType<NHibernateUnitOfWork>().As<IUnitOfWork>();
+
+            builder.RegisterType<NHibernateUnitOfWorkFactory>().As<IUnitOfWorkFactory>();
+
+            builder.RegisterGeneric(typeof(NHibernateRepositoryBase<>)).As(typeof(IRepository<>));
+
+            builder.Register(ctx =>
+            {
+                var nhInitializer = ctx.Resolve<INHibernateInitializer>();
+                ISessionFactory sessionFactory = nhInitializer.GetConfiguration().BuildSessionFactory();
+                return sessionFactory;
+            })
+                   .As<ISessionFactory>()
+                   .SingleInstance();
+
+            return builder;
+        }
+    }
+}

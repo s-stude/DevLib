233ee16b96be68c0629f6c8bf074ed068ff6a1b9
Akim Khalilov <Akim.Khalilov@gmail.com>
2013-03-05 22:54:39 +0200

    Adding IOC Registry assembly

---
+++ AutofacCommandHandlerFactory.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using DevLib.Infrastructure.Commands;
+using JetBrains.Annotations;
+
+namespace DevLib.Infrastructure.AutofacRegistry
+{
+    [UsedImplicitly]
+    public class AutofacCommandHandlerFactory : ICommandHandlerFactory
+    {
+        private readonly IComponentContext _componentContext;
+
+        public AutofacCommandHandlerFactory(IComponentContext componentContext)
+        {
+            _componentContext = componentContext;
+        }
+
+        public ICommandHandler<TCommand> Create<TCommand>() where TCommand : ICommand
+        {
+            return _componentContext.Resolve<ICommandHandler<TCommand>>();
+        }
+    }
+}
---
+++ ContainerBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using DevLib.Infrastructure.Queries.Impl;
+
+namespace DevLib.Infrastructure.AutofacRegistry
+{
+    public static class ContainerBuilderExtensions
+    {
+        public static ContainerBuilder RegisterInfrastructureCommandQueryComponents(this ContainerBuilder builder)
+        {
+            builder.RegisterGeneric(typeof (QueryFor<>))
+                .AsImplementedInterfaces()
+                .InstancePerDependency();
+
+            builder.RegisterType<AutofacCommandHandlerFactory>()
+                .AsImplementedInterfaces()
+                .InstancePerDependency();
+
+            builder.RegisterType<AutofacQueryFactory>()
+                .AsImplementedInterfaces()
+                .InstancePerDependency();
+
+            builder.RegisterType<AutofacQueryBuilder>()
+                .AsImplementedInterfaces()
+                .InstancePerDependency();
+
+            return builder;
+        }
+    }
+}

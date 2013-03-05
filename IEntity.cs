019f8cae0d9cc5512cbe6023b5b4d3a82da65768
Akim Khalilov <Akim.Khalilov@gmail.com>
2013-03-05 22:43:43 +0200

    Creating Domain assembly

---
+++ IEntity.cs
@@ -0,0 +1,7 @@
+namespace DevLib.Domain
+{
+    public interface IEntity
+    {
+        int Id { get; }
+    }
+}
---
+++ IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DevLib.Domain
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ///<summary>
+        /// Сохранить изменения в базу
+        ///</summary>
+        void Commit();
+    }
+}

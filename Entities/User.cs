d776a099d9415ad5ba72a52804d620066481e8a1
Akim Khalilov <Akim.Khalilov@gmail.com>
2013-04-21 22:40:17 +0300

    Social network login. New entities / fields + mapping and migrations. Commands and queries to work with user's data. Simple UI

--- Entities/User.cs
+++ Entities/User.cs
@@ -1,17 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using DevLib.Domain;
+using DevLib.Security.Enums;
 
 namespace DevLib.Security.Entities
 {
     public class User : IEntity
     {
-        public virtual int Id { get; protected set; }
+        private readonly ICollection<SocialNetworkAccount> _socialNetworkAccounts;
+
+        public virtual Guid Id { get; protected set; }
         public virtual string Login { get; protected set; }
         public virtual Password Password { get; protected set; }
+        public virtual string DisplayName { get; protected set; }
+        public virtual string Email { get; set; }
 
+
+        public virtual ICollection<SocialNetworkAccount> SocialNetworkAccounts
+        {
+            get { return _socialNetworkAccounts; }
+        }
+        
         protected User()
         {
-
+            _socialNetworkAccounts = new List<SocialNetworkAccount>();
         }
 
         public User(string login)
@@ -24,12 +37,41 @@ namespace DevLib.Security.Entities
 
         public virtual void SetPassword(string newPassword)
         {
-            Password = new Password(this, newPassword);
+            if (string.IsNullOrWhiteSpace(newPassword) == false)
+            {
+                Password = new Password(this, newPassword);
+            }
         }
 
         public virtual bool VerifyPassword(string passwordToVerify)
         {
             return Password.Verify(passwordToVerify);
+        }
+
+        public virtual void AddSocialNetworkAccount(SocialAuthProvider provider, string providerUserId)
+        {
+            if (_socialNetworkAccounts.All(x => x.Provider != provider))
+            {
+                var socialNetworkAccount = new SocialNetworkAccount(provider, this, providerUserId);
+
+                _socialNetworkAccounts.Add(socialNetworkAccount);
+            }
+        }
+
+        public virtual void ChangeDisplayName(string displayName)
+        {
+            if (string.IsNullOrWhiteSpace(displayName) == false)
+            {
+                DisplayName = displayName;
+            }
+        }
+
+        public virtual void ChangeEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email) == false)
+            {
+                Email = email;
+            }
         }
     }
 }
